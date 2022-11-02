965f84dbfc0a7af7653ceb1b438e8487f1e915cb
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 16:00:30 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count =0; 
+
+  function incrementCount() {
+    count +=1;
+  }
+  
+  let count2 =0; 
+
+  function incrementCount2() {
+    count2 +=5;
+  }
+</script>
+
+<!--minimum code while it's still working-->
+<button on:click={incrementCount}>
+  {count}
+</button> <hr>
+
+<!-- if you click more than 1 it shows clicked times(in plural)-->
+<button on:click={incrementCount2}>
+  Clicked {count2} {count2  === 1 ? 'time':'times'}
+</button> <hr>
